088a1ba4efdf1a3382e1173c1fe61ab7edb17ad4
Yanis Fourel <yanis.fourel42@proton.me>
2025-01-09 19:20:12 +0100

    aoc 2023

--- main.go
+++ main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println(line)
+	}
+
+	err = scanner.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
