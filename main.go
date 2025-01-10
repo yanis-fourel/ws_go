2d7c1f6717062af1c9ee5c9c434aa5d5fbaeb766
Yanis Fourel <yanis.fourel42@proton.me>
2025-01-10 09:17:47 +0100

    Initial myredis setup

--- main.go
+++ main.go
@@ -1,1 +1,49 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+const (
+	HOST = "localhost"
+	PORT = "6379"
+	TYPE = "tcp"
+)
+
+func main() {
+	listen, err := net.Listen(TYPE, HOST+":"+PORT)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+	// close listener
+	defer listen.Close()
+
+	for {
+		conn, err := listen.Accept()
+		if err != nil {
+			log.Fatal(err)
+			os.Exit(1)
+		}
+		go handleRequest(conn)
+	}
+}
+
+func handleRequest(conn net.Conn) {
+	// incoming request
+	buffer := make([]byte, 1024)
+	_, err := conn.Read(buffer)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// write data to response
+	responseStr := fmt.Sprintf("Received %v", string(buffer[:]))
+	conn.Write([]byte(responseStr))
+
+	// close conn
+	conn.Close()
+}
